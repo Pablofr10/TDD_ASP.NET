421e2e3a72227e3e5bd461769e014fba800a0e9c
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-30 22:30:44 -0300

    Criando primeiro teste domínio

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            // arrange
+            const string nome = "Informática Básica";
+            const double cargaHoraria = 80;
+            const string publicAlvo = "Estudantes";
+            const double valor = 950;
+
+            // action
+            var curso = new Curso(nome, cargaHoraria, publicAlvo, valor);
+
+
+            // assert
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, string publicAlvo, double valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+    }
+}
