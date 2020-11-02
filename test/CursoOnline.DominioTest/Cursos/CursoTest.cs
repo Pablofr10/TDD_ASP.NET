3251ddd05c333e42e64adb09b4d3de4a6f49a463
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-11-02 16:18:17 -0300

    Refatorando testes.

--- test/CursoOnline.DominioTest/Cursos/CursoTest.cs
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Bogus;
+using CursoOnline.Dominio.Cursos;
+using CursoOnline.Dominio.Enumerators;
 using CursoOnline.DominioTest._Builders;
 using CursoOnline.DominioTest._Util;
 using ExpectedObjects;
@@ -22,6 +24,7 @@ namespace CursoOnline.DominioTest.Cursos
             _output = output;
             _output.WriteLine("Contrutor sendo executado");
             var faker = new Faker();
+
 
             _nome = faker.Random.Word();
             _cargaHoraria = faker.Random.Int();
@@ -93,46 +96,5 @@ namespace CursoOnline.DominioTest.Cursos
         }
 
 
-    }
-
-    public enum PublicoAlvo
-    {
-        Estudante,
-        Universitario,
-        Empregado,
-        Empreendedor
-    }
-
-    public class Curso
-    {
-        public Curso(string nome, string descricao, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
-        {
-            if (string.IsNullOrEmpty(nome))
-            {
-                throw new ArgumentException("Nome Inválido");
-            }
-
-            if (cargaHoraria < 1)
-            {
-                throw new ArgumentException("Carga horária inválida");
-            }
-
-            if (valor < 1)
-            {
-                throw new ArgumentException("Valor inválido");
-            }
-
-            Nome = nome;
-            Descricao = descricao;
-            CargaHoraria = cargaHoraria;
-            PublicoAlvo = publicoAlvo;
-            Valor = valor;
-        }
-
-        public string Nome { get; private set; }
-        public string Descricao { get; private set; }
-        public double CargaHoraria { get; private set; }
-        public PublicoAlvo PublicoAlvo { get; private set; }
-        public double Valor { get; private set; }
     }
 }
--- test/CursoOnline.DominioTest/_Builders/CursoBuidler.cs
+++ test/CursoOnline.DominioTest/_Builders/CursoBuidler.cs
@@ -1,4 +1,5 @@
-﻿using CursoOnline.DominioTest.Cursos;
+﻿using CursoOnline.Dominio.Cursos;
+using CursoOnline.Dominio.Enumerators;
 
 namespace CursoOnline.DominioTest._Builders
 {
